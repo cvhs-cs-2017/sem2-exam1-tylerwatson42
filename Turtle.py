9a4ec75b4261292dad8486f87a3dabe664442ece
tylerwatson42 <watsonty101800@cvh.rusd.us>
2017-02-01 10:56:03 -0800

    part 1 turtle done

--- Turtle.py
+++ Turtle.py
@@ -2,9 +2,30 @@
 import turtle
 t = turtle.Turtle()
 for i in range(4):
-    t.forward(100)
+    t.forward(90)
     t.left(90)
-
+t.left(45)
+t.forward(90)
+t.left(45)
+t.forward(90)
+for i in range(3):
+    t.right(90)
+    t.forward(90)
+t.right(180)
+t.forward(90)
+t.right(135)
+t.forward(90)
+t.right(45)
+t.forward(90)
+t.right(90)
+t.forward(90)
+t.right(45)
+t.forward(90)
+t.right(45)
+t.forward(90)
+t.right(135)
+t.forward(90)
+input()
 
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the
 file MyFile.py"""
