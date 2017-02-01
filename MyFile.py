53c672312f836da9d4752f49d526f440024cf83f
tylerwatson42 <watsonty101800@cvh.rusd.us>
2017-02-01 11:28:15 -0800

    My file created

---
+++ MyFile.py
@@ -0,0 +1,5 @@
+"""
+# AddTen(n):  Adds 10 to the parameter n and returns the result
+# DrawRectangle(Anyturtle, l, w):  Self Explanitory
+# DrawPoly(Anyturtle, n):  Will draw a regular polygon with 'n' sides of
+* You should select the size of the polygon so that it always fits in the screen """
