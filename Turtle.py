0120f43f6ff7a11d8db5706c179db5e070dc1fe4
tylerwatson42 <watsonty101800@cvh.rusd.us>
2017-02-01 10:16:47 -0800

    not done

--- Turtle.py
+++ Turtle.py
@@ -1,7 +1,9 @@
 """Create a Turtle Program that will draw a 3-dimensional cube"""
-
-
-
+import turtle
+t = turtle.Turtle()
+for i in range(4):
+    t.forward(100)
+    t.left(90)
 
 
 """Import and Call the DrawRectangle(Anyturtle, l, w) function from the
