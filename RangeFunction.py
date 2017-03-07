0b2f719f365877d928054afc14625eedd37a99c7
tylerwatson42 <tywat10000@gmail.com>
2017-03-07 11:15:33 -0800

    done

--- RangeFunction.py
+++ RangeFunction.py
@@ -11,10 +11,10 @@ for i in range(40, 0, -5):
 """Write a program that will count print all the multiples of (n) where n is
 taken from user input.  Include necessary print statements."""
 print('Give me a number:')
-x = 1
+t = 1
 n = int(input())
 for i in range(10):
 
-    y = n * x
-    x = x + 1
+    y = n * t
+    t = t + 1
     print(y)
