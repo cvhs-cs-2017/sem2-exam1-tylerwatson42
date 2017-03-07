fbb9cdc1255388e55619a41b8683be55281bd62d
tylerwatson42 <tywat10000@gmail.com>
2017-03-07 10:53:57 -0800

    Finished

--- Function.py
+++ Function.py
@@ -1,13 +1,17 @@
 """Define a function that will take a parameter, n, and triple it and return
 the result"""
-n = input()
-parameter = n
-s = int(n*3)
-answer = int(s*3)
-print(answer)
+n = 3
+def TripFunc(n):
+    a = n**3
+    return a
 
+print(TripFunc(n))
 
 
 """Write a program that will prompt the user for an input value (n) and print
 the result of 3n by calling the function defined above.  Make sure you include
 the necessary print statements and address any issues with whitespace. """
+
+n = int(input("Type in a number:"))
+
+print(TripFunc(n))
